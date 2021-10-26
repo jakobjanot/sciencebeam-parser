a159252ef602026ff00351649aa9d29dbbd98882
Daniel Ecer <de-code@users.noreply.github.com>
2021-10-26 21:50:50 +0100

    moved utility functions to flask utils module

--- sciencebeam_parser/service/blueprints/api.py
+++ sciencebeam_parser/service/blueprints/api.py
@@ -1,13 +1,11 @@
-# pylint: disable=too-many-lines
 import logging
 from tempfile import TemporaryDirectory
 from pathlib import Path
-from typing import Callable, Iterable, List, NamedTuple, Optional, Type, TypeVar
+from typing import Callable, Iterable, List, Optional, Type, TypeVar
 from zipfile import ZipFile
 
 from flask import Blueprint, jsonify, request, Response, url_for
 from flask.helpers import send_file
-from werkzeug.exceptions import BadRequest
 from lxml import etree
 
 from sciencebeam_trainer_delft.sequence_labelling.reader import load_data_crf_lines
@@ -45,11 +43,14 @@ from sciencebeam_parser.document.tei_document import get_tei_for_semantic_docume
 from sciencebeam_parser.processors.fulltext.config import RequestFieldNames
 from sciencebeam_parser.resources.xslt import TEI_TO_JATS_XSLT_FILE
 from sciencebeam_parser.transformers.xslt import XsltTransformerWrapper
-from sciencebeam_parser.utils.flask import assert_and_get_first_accept_matching_media_type
+from sciencebeam_parser.utils.flask import (
+    assert_and_get_first_accept_matching_media_type,
+    get_data_wrapper_with_improved_media_type_or_filename,
+    get_required_post_data,
+    get_required_post_data_wrapper
+)
 from sciencebeam_parser.utils.media_types import (
-    MediaTypes,
-    guess_extension_for_media_type,
-    guess_media_type_for_filename
+    MediaTypes
 )
 from sciencebeam_parser.utils.text import normalize_text, parse_comma_separated_value
 from sciencebeam_parser.utils.tokenizer import get_tokenized_tokens
@@ -79,9 +80,6 @@ class ModelOutputFormats:
     RAW_DATA = 'raw_data'
 
 
-DEFAULT_FILENAME = 'file'
-
-
 DEFAULT_MODEL_OUTPUT_FORMAT = TagOutputFormats.JSON
 VALID_MODEL_OUTPUT_FORMATS = {
     ModelOutputFormats.RAW_DATA,
@@ -89,60 +87,6 @@ VALID_MODEL_OUTPUT_FORMATS = {
     TagOutputFormats.DATA,
     TagOutputFormats.XML
 }
-
-
-class SourceDataWrapper(NamedTuple):
-    data: bytes
-    media_type: str
-    filename: Optional[str] = None
-
-
-def get_optional_post_data_wrapper() -> SourceDataWrapper:
-    if not request.files:
-        return SourceDataWrapper(
-            data=request.data,
-            media_type=request.mimetype,
-            filename=request.args.get('filename')
-        )
-    supported_file_keys = ['file', 'input']
-    for name in supported_file_keys:
-        if name not in request.files:
-            continue
-        uploaded_file = request.files[name]
-        data = uploaded_file.stream.read()
-        return SourceDataWrapper(
-            data=data,
-            media_type=uploaded_file.mimetype,
-            filename=uploaded_file.filename
-        )
-    raise BadRequest(
-        f'missing file named one pf "{supported_file_keys}", found: {request.files.keys()}'
-    )
-
-
-def get_data_wrapper_with_improved_media_type_or_filename(
-    data_wrapper: SourceDataWrapper
-) -> SourceDataWrapper:
-    if not data_wrapper.filename:
-        return data_wrapper._replace(filename='%s%s' % (
-            DEFAULT_FILENAME, guess_extension_for_media_type(data_wrapper.media_type) or ''
-        ))
-    if data_wrapper.media_type == 'application/octet-stream':
-        media_type = guess_media_type_for_filename(data_wrapper.filename)
-        if media_type:
-            return data_wrapper._replace(media_type=media_type)
-    return data_wrapper
-
-
-def get_required_post_data_wrapper() -> SourceDataWrapper:
-    data_wrapper = get_optional_post_data_wrapper()
-    if not data_wrapper.data:
-        raise BadRequest('no contents')
-    return data_wrapper
-
-
-def get_required_post_data() -> bytes:
-    return get_required_post_data_wrapper().data
 
 
 def get_typed_request_arg(
--- sciencebeam_parser/utils/flask.py
+++ sciencebeam_parser/utils/flask.py
@@ -1,13 +1,74 @@
 import logging
-from typing import Sequence
+from typing import NamedTuple, Optional, Sequence
 
 from flask import request
 from werkzeug.exceptions import BadRequest
 
-from sciencebeam_parser.utils.media_types import get_first_matching_media_type
+from sciencebeam_parser.utils.media_types import (
+    get_first_matching_media_type,
+    guess_extension_for_media_type,
+    guess_media_type_for_filename
+)
 
 
 LOGGER = logging.getLogger(__name__)
+
+
+DEFAULT_FILENAME = 'file'
+
+
+class SourceDataWrapper(NamedTuple):
+    data: bytes
+    media_type: str
+    filename: Optional[str] = None
+
+
+def get_optional_post_data_wrapper() -> SourceDataWrapper:
+    if not request.files:
+        return SourceDataWrapper(
+            data=request.data,
+            media_type=request.mimetype,
+            filename=request.args.get('filename')
+        )
+    supported_file_keys = ['file', 'input']
+    for name in supported_file_keys:
+        if name not in request.files:
+            continue
+        uploaded_file = request.files[name]
+        data = uploaded_file.stream.read()
+        return SourceDataWrapper(
+            data=data,
+            media_type=uploaded_file.mimetype,
+            filename=uploaded_file.filename
+        )
+    raise BadRequest(
+        f'missing file named one pf "{supported_file_keys}", found: {request.files.keys()}'
+    )
+
+
+def get_data_wrapper_with_improved_media_type_or_filename(
+    data_wrapper: SourceDataWrapper
+) -> SourceDataWrapper:
+    if not data_wrapper.filename:
+        return data_wrapper._replace(filename='%s%s' % (
+            DEFAULT_FILENAME, guess_extension_for_media_type(data_wrapper.media_type) or ''
+        ))
+    if data_wrapper.media_type == 'application/octet-stream':
+        media_type = guess_media_type_for_filename(data_wrapper.filename)
+        if media_type:
+            return data_wrapper._replace(media_type=media_type)
+    return data_wrapper
+
+
+def get_required_post_data_wrapper() -> SourceDataWrapper:
+    data_wrapper = get_optional_post_data_wrapper()
+    if not data_wrapper.data:
+        raise BadRequest('no contents')
+    return data_wrapper
+
+
+def get_required_post_data() -> bytes:
+    return get_required_post_data_wrapper().data
 
 
 def get_request_accept_media_types() -> Sequence[str]:
